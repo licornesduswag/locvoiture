59ca6fb5fce04f3e708b5bbda01d5a18c874d367
Romain Porte <microjoe@mailoo.org>
2016-01-20 15:43:32 +0100

    Premier jet driver Postgresql

---
+++ src/main/java/fr/ensim/locvoiture/model/PostgresBDD.java
@@ -0,0 +1,145 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2016 Romain Porte (MicroJoe) microjoe at mailoo.org.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package fr.ensim.locvoiture.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Romain Porte (MicroJoe) microjoe at mailoo.org
+ */
+public class PostgresBDD implements BDDInterface {
+    
+    private Connection conn;
+    private Statement stmt;
+
+    public PostgresBDD() {
+        try {
+            Class.forName("org.postgresql.Driver");
+            System.out.println("Driver PostgreSQL chargé !");
+            
+            conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/microjoe", "microjoe", "");
+            stmt = conn.createStatement();
+            
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(PostgresBDD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }   
+
+    @Override
+    public void saveContrat(Contrat contrat) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void removeContrat(Contrat contrat) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Contrat> listContrats() {
+        try {
+            ResultSet result = stmt.executeQuery("SELECT * FROM contrats NATURAL JOIN voitures");
+            ArrayList<Contrat> contrats = new ArrayList<>();
+            
+            while (result.next()) {
+                System.out.println(result.getString("matricule"));
+                
+                Voiture v = new Voiture(result.getString("matricule"), result.getString("marque"), result.getInt("kilometrage"), result.getString("couleur"), result.getInt("id_voiture"));
+                Contrat c = new Contrat(v, result.getDate("date_debut"), result.getDate("date_fin"), result.getInt("kilometrage_debut"), result.getInt("kilometrage_fin"), result.getInt("id_contrat"));
+                
+                contrats.add(c);
+            }
+            
+            return contrats;
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(PostgresBDD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return null;
+    }
+
+    @Override
+    public List<Voiture> listVoitures() {
+        try {
+            ResultSet result = stmt.executeQuery("SELECT * FROM voitures");
+            
+            List<Voiture> voitures = new ArrayList<>();
+            
+            while (result.next()) {
+                Voiture v = new Voiture(result.getString("matricule"), result.getString("marque"), result.getInt("kilometrage"), result.getString("couleur"), result.getInt("id_voiture"));
+                voitures.add(v);
+            }
+            
+            return voitures;
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(PostgresBDD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return null;
+    }
+
+    @Override
+    public void saveClient(Client client) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Client> listClients() {
+        try {
+            ResultSet result = stmt.executeQuery("SELECT * FROM clients");
+            
+            List<Client> clients = new ArrayList();
+            
+            while(result.next()) {
+                InfoPermis ip = new InfoPermis(result.getInt("permis_numero"), result.getDate("permis_date_delivrance"), result.getString("permis_lieu_delivrance"), result.getString("permis_ville_delivrance"));
+                Client c = new Client(result.getString("nom"), result.getString("prenom"), result.getDate("date_naissance"), result.getString("lieu_naissance"), ip);
+                
+                // Ne fonctionne pas ?
+                ip.setId(result.getInt("id_client"));
+                
+                clients.add(c);
+            }
+            
+            return clients;
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(PostgresBDD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return null;
+    }
+
+}
