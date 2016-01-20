0dc3885379bc241d04f30b1769891fe54df3f941
Romain Porte <microjoe@mailoo.org>
2016-01-20 14:19:21 +0100

    Ajout des premiers modeles

---
+++ src/main/java/fr/ensim/locvoiture/model/Client.java
@@ -0,0 +1,106 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2016 Romain Porte (MicroJoe) microjoe at mailoo.org.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package fr.ensim.locvoiture.model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Romain Porte (MicroJoe) microjoe at mailoo.org
+ */
+public class Client {
+    /**
+     * ID dans la BDD du client, -1 si pas défini (utiliser auto-increment)
+     */
+    private int id = -1;
+    
+    private String nom;
+    private String prenom;
+    
+    private Date dateNaissance;
+    private String lieuNaissance;
+    
+    private InfoPermis infoPermis;
+
+    public Client(String nom, String prenom, Date dateNaissance, String lieuNaissance, InfoPermis infoPermis) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.dateNaissance = dateNaissance;
+        this.lieuNaissance = lieuNaissance;
+        this.infoPermis = infoPermis;
+    }
+    
+    // Getters & setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public Date getDateNaissance() {
+        return dateNaissance;
+    }
+
+    public void setDateNaissance(Date dateNaissance) {
+        this.dateNaissance = dateNaissance;
+    }
+
+    public String getLieuNaissance() {
+        return lieuNaissance;
+    }
+
+    public void setLieuNaissance(String lieuNaissance) {
+        this.lieuNaissance = lieuNaissance;
+    }
+
+    public InfoPermis getInfoPermis() {
+        return infoPermis;
+    }
+
+    public void setInfoPermis(InfoPermis infoPermis) {
+        this.infoPermis = infoPermis;
+    }
+
+    
+}
